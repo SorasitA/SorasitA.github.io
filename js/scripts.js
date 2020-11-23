e0e7b74af21c7c28680f3d6cdd821a14f8361147
Sorasit Arsirapongphisit <thefallenpoom@gmail.com>
2020-11-24 00:04:32 +0700

    init

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-function getCompanyName() {
-    let c = document.getElementById("companyName")
-    let name = prompt("Enter Caompany Name")
-    console.log("User has input ",name)
-    c.innerText = name
-}
